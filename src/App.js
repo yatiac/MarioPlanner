e1bb422f5c42114e12cff90f67f441a5689a9d92
Yatiac <master.152@gmail.com>
2020-02-21 22:30:36 -0500

    Testing Actions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import NavBar from './components/layouts/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
--- src/components/layouts/SignedInLinks.js
+++ src/components/layouts/SignedInLinks.js
@@ -7,7 +7,7 @@ const SignedInLinks = (props) => {
     return (
         <ul className="right">
             <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><a onClick={ props.signOut }>Log Out</a></li>
+            <li><a href="#" onClick={ props.signOut }>Log Out</a></li>
             <li><NavLink to='/' className='btn btn-floating pink ligthen-1'>{ props.profile.initials }</NavLink></li>
         </ul>
     )
